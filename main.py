c75e468dbea9afb49a1e5acd5397f4122c1dacad
HQQTr1ndec <7evennns4@gmail.com>
2025-05-06 21:32:50 +0300

    add mathBrains vs Comp aio

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+points = {
+    "comp": 0,
+    "player": 0
+}
+
+
+
+while True:
+    a =random.randint(10,90)
+    b =random.randint(10,90)
+    op =random.choice(("+", "-"))
+    if op =="+":
+        ans = a + b
+        user = input(f"{a} + {b} = ")
+    else:
+        ans = a - b
+        user = input(f"{a} - {b} = ")
+
+    if user == str(ans):
+        print("Верно!!!")
+        points["player"]+=1
+    else:
+        print("ТЫ ПОТЕРПЕЛ НЕУДАЧУ!!!")
+        points["comp"]+=1
+    print(f'Счет: {points["player"]}:{points["comp"]}')
+
+
+
+
